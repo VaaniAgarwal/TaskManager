130048640db3b1d36ca6234e423c4ed90baa6a8f
Vaani Agarwal <vaaniagarwal0307@gmail.com>
2025-02-04 16:45:31 +0530

    Changes in main

--- src/main/java/com/mycompany/taskmanagerfx/TaskManagerFX.java
+++ src/main/java/com/mycompany/taskmanagerfx/TaskManagerFX.java
@@ -11,6 +11,6 @@ package com.mycompany.taskmanagerfx;
 public class TaskManagerFX {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome");
     }
 }
