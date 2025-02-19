ab9126e7e161aad6383fcc1bffbbe76b7d82f948
Vaani Agarwal <vaaniagarwal0307@gmail.com>
2025-02-19 22:49:31 +0530

    Initial Commit
    Task Manager to add , delete and view tasks.

--- src/main/java/com/mycompany/taskmanagerfx/TaskManagerFX.java
+++ src/main/java/com/mycompany/taskmanagerfx/TaskManagerFX.java
@@ -3,19 +3,28 @@
  */
 
 package com.mycompany.taskmanagerfx;
-
+import com.mycompany.taskmanagerfx.controller.TaskController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+  
 /**
  *
  * @author VAANI
  */
-public class TaskManagerFX {
-
+public class TaskManagerFX extends Application {
+    @Override
+    public void start(Stage stage) 
+    {
+        TaskController controller=new TaskController();
+        VBox view=controller.getView();
+        Scene scene=new Scene(view,400,300);
+        stage.setTitle("Task Manager");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome");
-        int a;
-        int b=4;
-        int c=9;
-        a=b+c;
-        System.out.println(+a);
+        launch(args);
     }
 }
--- src/main/java/com/mycompany/taskmanagerfx/controller/TaskController.java
+++ src/main/java/com/mycompany/taskmanagerfx/controller/TaskController.java
@@ -3,11 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.taskmanagerfx.controller;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 /**
  *
  * @author VAANI
  */
 public class TaskController {
-    
+    private final Label label;
+    private final TextField task;
+    private final Button addtask;
+    private final Button deletetask;
+    private final ListView<String> Viewtask;
+    private final VBox view;
+    public TaskController()
+    {
+        label = new Label("Task Manager");
+        task=new TextField();
+        task.setPromptText("Enter task name");
+        addtask=new Button("Add New Task");
+        deletetask=new Button("Delete a Task");
+        Viewtask=new ListView<>();
+        view=new VBox(10);
+        view.getChildren().addAll(label,task,addtask,deletetask,Viewtask);
+    }
+    public VBox getView()
+    {
+        return view;
+    }
 }
